57881792ff3f2143198517b352b7c796bb6cf60b
zsxdev <zsx.lolsz@gmail.com>
2018-07-24 12:21:37 -0400

    Using Hnews api to retrieve news.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author : 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author : 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`
 
 class App extends Component {
 
@@ -27,18 +16,22 @@ class App extends Component {
     super(props);
 
     this.state ={
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
     
     // Doing this.onDismiss = () => {} is a bad practice
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
   }
   // Logic of class methods should be defined outside of the constructor
   // ** We can auto-bound automatically without binding by using arrow functiosn
   
+  setSearchTopStories(result){
+    this.setState({ result });
+  }
 
   onDismiss(id){
     const isNotId = item => item.objectID != id;
@@ -56,8 +49,19 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopStories(result))
+    .catch(error => error);
+  }
+
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if(!result) { return null;}
+
     return (
       <div className="page">
         <div className="interactions">
@@ -69,7 +73,7 @@ class App extends Component {
           </Search>
       </div>
       <Table
-        list = {list} 
+        list = {result.hits} 
         pattern = {searchTerm}
         onDismiss = {this.onDismiss}
         isSearched = {this.isSearched}
@@ -78,6 +82,7 @@ class App extends Component {
     );
   }
 }
+
 // Search is a  Functional Stateless Component.
 const Search = ({value , onChange, children}) =>
   <form>
@@ -93,19 +98,20 @@ class Table extends Component {
   render(){
     const {list, pattern, onDismiss, isSearched,} = this.props;
     return (
-      <div>
+      <div className="table">
         {
           list.filter(isSearched(pattern)).map(item =>
-            <div key={item.objectID}>
-              <span>
+            <div key={item.objectID} className="table-row">
+              <span style={{ width: '40%' }}>
                 <a href={item.url}> {item.title}</a>
               </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
+              <span style={{ width: '30%' }}>{item.author}</span>
+              <span style={{ width: '10%' }}>{item.num_comments}</span>
+              <span style={{ width: '10%' }}>{item.points}</span>
+              <span style={{ width: '10%' }}>
                 <Button 
                   onClick = {() => onDismiss(item.objectID)}
+                  className="button-inline"
                 >
                   Dismiss
                 </Button>
